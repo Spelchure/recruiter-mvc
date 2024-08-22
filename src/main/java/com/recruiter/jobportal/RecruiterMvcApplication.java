35cee71d2177d07995cd1a28ef2a5489526bf15b
Spelchure <alperenbayramoglu2@gmail.com>
2024-08-23 00:06:38 +0300

    initial: initial commit

---
+++ src/main/java/com/recruiter/jobportal/RecruiterMvcApplication.java
@@ -0,0 +1,13 @@
+package com.recruiter.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruiterMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruiterMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recruiter/jobportal/RecruiterMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruiter.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruiterMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
